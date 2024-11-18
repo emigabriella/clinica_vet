40d080c4b0f3ad228ba61e116fbc249b86a7eada
emiisabella98 <emiisabella98@gmail.com>
2024-11-17 21:19:06 -0600

    pantallas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { InicioComponent } from './componentes/inicio/inicio.component';
+import { ErrorComponent } from './componentes/error/error.component';
 
 
 export const routes: Routes = [
-  {path: "", component: InicioComponent}
+  {path: "", component: InicioComponent},
+  {path: "**", component: ErrorComponent}
 ];
